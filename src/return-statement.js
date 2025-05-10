6b0a8c4799a4aa207a0c81568ebf6b393ccb3e08
Zevi Liao <jointjm@gmail.com>
2025-05-11 02:12:07 +0800

    Update code

--- src/return-statement.js
+++ src/return-statement.js
@@ -6,21 +6,22 @@ const sayHello = () => {
 // const sayHello = () => "[return] hello2 , John";
 
 // // 寫法 3
-// const sayHello = () => 
+// const sayHello = () =>
 //   "[return] hello3 , John";
 
 // const sayHello = () => (
-//   "[return] hello3 , John";
+//   "[return] hello3 , John"
 // )
 
 // error
 // const sayHello = () => {
-//   return "[return] hello4 , John";
+//   return 
+//   "[return] hello4 , John";
 // };
 
 // // no return
-// const sayHello = () => {
-//   "[return] hello5 , John";
-// };
+const sayHello = () => {
+  "[return] hello5 , John";
+};
 
 console.log(sayHello());
